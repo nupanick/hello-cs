c6683b66da873f9a981ddfcaf4fe5492b19a2b54
Nicholas Lamicela <nupanick@gmail.com>
2016-03-10 18:00:33 +0000

    Well, that wasn't so bad.

---
+++ hello.cs
@@ -0,0 +1,15 @@
+// "Hello World!" program in C#
+// Nick Lamicela 2016
+
+using System;
+
+namespace HelloWorld
+{
+	public class Hello
+	{
+		public static void Main ()
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
